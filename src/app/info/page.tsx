3f93be67482798f3e12da6045dc41ed7223ac2a0
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-02-15 22:16:19 +0700

    fix: benerin /info dikit

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -9,22 +9,24 @@ import { FaChevronLeft } from "react-icons/fa";
 function page() {
   return (
     <Layout withFooter={true} withNavbar={false} transparentOnTop={false}>
-      <div className="mb-20">
-        <HeroSection />
-      </div>
-      <div className="flex flex-col justify-center mx-20 lg:mx-40 gap-12 mb-16">
-        <MediaPartner />
-        <About />
-        <Contact />
-        <ButtonLink
-          href="/"
-          className="w-28 flex gap-4 items-center"
-          variant="black"
-        >
-          <FaChevronLeft className="hover:text-primaryGreen transition-all duration-300" />
-          <p>Home</p>
-        </ButtonLink>
-      </div>
+      <main>
+        <div className="mb-20">
+          <HeroSection />
+        </div>
+        <div className="max-w-8xl flex flex-col gap-24 lg:gap-32 mt-24 lg:mt-32 px-6 lg:px-32 mb-16">
+          <MediaPartner />
+          <About />
+          <Contact />
+          <ButtonLink
+            href="/"
+            className="w-28 flex gap-4 items-center"
+            variant="black"
+          >
+            <FaChevronLeft className="hover:text-primaryGreen transition-all duration-300" />
+            <p>Home</p>
+          </ButtonLink>
+        </div>
+      </main>
     </Layout>
   );
 }
