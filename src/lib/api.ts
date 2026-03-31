baddd8f33d217ece1877f15c609477df809eb323
Fastering18 <brahmana165@gmail.com>
2026-04-01 00:42:14 +0700

    fix: api url again

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -7,9 +7,11 @@ import { UninterceptedApiError } from "@/types/api";
 const context = <GetServerSidePropsContext>{};
 
 export const baseURL =
-  process.env.NEXT_PUBLIC_RUN_MODE === "development"
+  (process.env.NEXT_PUBLIC_RUN_MODE === "development"
     ? process.env.NEXT_PUBLIC_API_URL_DEV
-    : process.env.NEXT_PUBLIC_API_URL_PROD;
+    : process.env.NEXT_PUBLIC_API_URL_PROD) ||
+  process.env.NEXT_PUBLIC_API_URL ||
+  process.env.APP_URL;
 
 export const api = axios.create({
   baseURL,
