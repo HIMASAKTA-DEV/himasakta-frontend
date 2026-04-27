b9e14f4d8e3c9e60056d68c35abf8fb3dc68a727
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2026-04-27 21:54:31 +0700

    fix: get to know limit now all event month

--- src/services/landing_page/GetToKnow.ts
+++ src/services/landing_page/GetToKnow.ts
@@ -2,11 +2,12 @@ import api from "@/lib/axios";
 import type { ApiResponse } from "@/types/api";
 import { MonthlyEvent } from "@/types/data/GetToKnow";
 
+// currently all event
 export const GetEventThisMonth = async (
   limit: number = 20,
 ): Promise<ApiResponse<MonthlyEvent[]>> => {
   const resp = await api.get<ApiResponse<MonthlyEvent[]>>(
-    `/monthly-event/this-month?limit=${limit}`,
+    `/monthly-event?limit=${limit}`,
   );
 
   return resp.data;
